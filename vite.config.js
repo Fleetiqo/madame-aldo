11a83f7c0bb68989023d157604d84af21568bd98
Fleetiqo <contact@fleetiqo.com>
2026-03-17 15:01:58 -0400

    a

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/madame-aldo/',
+  publicDir: 'assets',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: { port: 3000 }
 })
